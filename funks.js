15228715ba73133b2e5d7466e2c23014aa7e0ecd
vsuaste <suaste@cimat.mx>
2019-01-07 17:53:42 -0600

    JSDoc type documentation added

--- funks.js
+++ funks.js
@@ -6,24 +6,36 @@ const jsb = require('js-beautify').js_beautify;
 const {promisify} = require('util');
 const ejsRenderFile = promisify( ejs.renderFile );
 
+
+/**
+ *  Allowed type of associations classified accordingly the number of model involved
+ *  @constant
+ *  @type {object}
+ */
 associations_type = {
   "many" : ['sql_hasMany', 'sql_belongsToMany','cross_hasMany'],
   "one" : ['sql_hasOne', 'sql_belongsTo', 'cross_hasOne','cross_belongsTo']
 }
 
-/*
-association_storage = {
-  "sql" : ['sql_hasMany', 'sql_hasOne', 'sql_belongsTo', 'sql_belongsToMany'],
-  "cross" : ['cross_hasMany', 'cross_hasOne']
-}
-*/
-
+/**
+ * parseFile - Parse a json file
+ *
+ * @param  {string} jFile path wher json file is stored
+ * @return {object}       json file converted to js object
+ */
 parseFile = function(jFile){
   let data=fs.readFileSync(jFile, 'utf8');
   let words=JSON.parse(data);
   return words;
 }
 
+
+/**
+ * isEmptyObject - Determines if an object is empty
+ *
+ * @param  {object} obj Object to check if is empty
+ * @return {boolean}     False if 'obj' has at least one entry, true if the object is empty.
+ */
 isEmptyObject = function(obj){
     for(let key in obj) {
         if(obj.hasOwnProperty(key))
@@ -32,7 +44,13 @@ isEmptyObject = function(obj){
     return true;
 }
 
-// Generate the Javascript code (GraphQL-schema/resolvers/Sequelize-model) using EJS templates
+/**
+ * generateJs - Generate the Javascript code (GraphQL-schema/resolvers/Sequelize-model) using EJS templates
+ *
+ * @param  {string} templateName Name of the template to use
+ * @param  {object} options      Options that the template will use
+ * @return {string}              String of created file with specified template
+ */
 module.exports.generateJs = async function(templateName, options) {
   let renderedStr = await ejsRenderFile(__dirname + '/views/' +
     templateName +
@@ -41,6 +59,13 @@ module.exports.generateJs = async function(templateName, options) {
   return prettyStr;
 }
 
+
+/**
+ * attributesToString - Convert object attributes to a string separating by dots the key and value and by comma each attribute.
+ *
+ * @param  {object} attributes Object attributes to convert
+ * @return {string}            Converted object into a single string
+ */
 attributesToString = function(attributes){
   let str_attributes="";
   if(attributes==='undefined' || isEmptyObject(attributes)) return str_attributes;
@@ -54,6 +79,12 @@ attributesToString = function(attributes){
 }
 
 
+/**
+ * attributesArrayString - Get all attributes of type string
+ *
+ * @param  {object} attributes Object containing the attributes to parse
+ * @return {array}            Array of string containing only the name of the attributes which type is "string"
+ */
 attributesArrayString = function(attributes){
   let array_attributes = ["id"];
 
@@ -68,6 +99,13 @@ attributesArrayString = function(attributes){
 }
 
 
+
+/**
+ * writeSchemaCommons - Writes a 'commons.js' file into the given directory. This file contains
+ * general parts of the graphql schema that are common for all models.
+ *
+ * @param  {string} dir_write Path of the directory where to create the commons.js file
+ */
 writeSchemaCommons = function(dir_write){
 
   let commons = `module.exports = \`
@@ -104,6 +142,13 @@ writeSchemaCommons = function(dir_write){
     });
 }
 
+
+/**
+ * writeIndexModelsCommons - Writes a 'index.js' file of all models stored inside the given directory. This file
+ * will allow to import all sequelize models before creating associations between models.
+ *
+ * @param  {string} dir_write Path of the directory where to create the index.js file
+ */
 writeIndexModelsCommons = function(dir_write){
 
   let index =  `
@@ -140,8 +185,15 @@ writeIndexModelsCommons = function(dir_write){
     });
 }
 
-convertToType = function(many, model_name)
-{
+
+/**
+ * convertToType - Generate a string correspondant to the model type as needed for graphql schema.
+ *
+ * @param  {boolean} many       True if the field type in the schema corresponds to an array, false otherwise.
+ * @param  {type} model_name Name of the model.
+ * @return {string}            String indicating array or only the model name.
+ */
+convertToType = function(many, model_name){
   if(many)
   {
     return '[ '+ model_name +' ]';
@@ -150,8 +202,15 @@ convertToType = function(many, model_name)
   return model_name;
 }
 
-module.exports.getOptions = function(dataModel)
-{
+
+
+/**
+ * getOptions - Creates object with all extra info and with all data model info.
+ *
+ * @param  {object} dataModel object created from a json file containing data model info.
+ * @return {object}           Object with all extra info that will be needed to create files with templates.
+ */
+module.exports.getOptions = function(dataModel){
   //let dataModel = parseFile(json_file);
   //console.log(dataModel.associations);
   let opts = {
@@ -170,8 +229,16 @@ module.exports.getOptions = function(dataModel)
   return opts;
 }
 
-parseAssociations = function(associations, storageType)
-{
+
+/**
+ * parseAssociations - Parse associations of a given data model.
+ * Classification of associations will be accordingly to the type of association and storage type of target model.
+ *
+ * @param  {object} associations Description of each association
+ * @param  {string} storageType  Storage type(i.e. sql, webservice) where source model is stored.
+ * @return {object}              Object containing explicit information needed for generating files with templates.
+ */
+parseAssociations = function(associations, storageType){
   associations_info = {
     "schema_attributes" : {
       "many" : {},
@@ -231,6 +298,15 @@ parseAssociations = function(associations, storageType)
     return associations_info;
   }
 
+
+
+/**
+ * generateAssociationsMigrations - Create files for migrations of associations between models. It could be either
+ * creating a new column or creating a through table.
+ *
+ * @param  {object} opts      Object with options required for the template that creates migrations.
+ * @param  {string} dir_write Path where the the file will be written.
+ */
 generateAssociationsMigrations =  function( opts, dir_write){
 
     opts.associations.implicit_associations.belongsTo.forEach( async (assoc) =>{
@@ -263,8 +339,15 @@ generateAssociationsMigrations =  function( opts, dir_write){
     });
 }
 
-generateSection = async function(section, opts, dir_write )
-{
+
+ /**
+  * generateSection - Writes a file which contains a generated section. Each seaction has its own template.
+  *
+  * @param  {string} section   Name of section that will be generated (i.e. schemas, models, migrations, resolvers)
+  * @param  {object} opts      Object with options needed for the template that will generate the section
+  * @param  {string} dir_write Path (includin name of the file) where the generated section will be written as a file.
+  */
+generateSection = async function(section, opts, dir_write ){
   let generatedSection = await module.exports.generateJs('create-'+section ,opts);
   fs.writeFile(dir_write, generatedSection, function(err) {
     if (err)
@@ -274,22 +357,46 @@ generateSection = async function(section, opts, dir_write )
   });
 }
 
-createNameMigration = function(dir_write, model_name)
-{
+
+/**
+ * createNameMigration - Creates the name for the migration file accordingly to the time and date
+ * that the migration is created.
+ *
+ * @param  {string} dir_write  directory where code is being generated.
+ * @param  {string} model_name Name of the model.
+ * @return {string}            Path where generated file will be written.
+ */
+createNameMigration = function(dir_write, model_name){
   let date = new Date();
    date = date.toISOString().slice(0,19).replace(/[^0-9]/g, "");
   //return dir_write + '/migrations/' + date + '-create-'+model_name +'.js';
   return dir_write + '/migrations/' + date + '-'+model_name +'.js';
 }
 
+
+/**
+ * writeCommons - Write static files
+ *
+ * @param  {string} dir_write directory where code is being generated.
+ */
 writeCommons = function(dir_write){
   writeSchemaCommons(dir_write);
   writeIndexModelsCommons(dir_write);
 }
 
 
-module.exports.generateCode = function(json_dir, dir_write)
-{
+
+
+ /**
+  * generateCode - Given a set of json files, describing each of them a data model, this
+  * functions generate the code for a graphql server that will handle CRUD operations.
+  * The generated code consists of four sections: sequelize models, migrations, resolvers and
+  * graphql schemas.
+  *
+  * @param  {string} json_dir  Directory where the json files are stored.
+  * @param  {string} dir_write Directory where the generated code will be written.
+  */
+module.exports.generateCode = function(json_dir, dir_write){
   console.log("Generating files...");
   dir_write = (dir_write===undefined) ? __dirname : dir_write;
   let sections = ['schemas', 'resolvers', 'models', 'migrations'];
