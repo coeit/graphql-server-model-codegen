4eda39fd708d2b9dc75c049d6bff624507e15f3a
vsuaste <suaste@cimat.mx>
2019-01-13 16:07:11 -0600

    jsdoc documentation for created migrations

--- test/data-test.js
+++ test/data-test.js
@@ -710,8 +710,18 @@ module.exports = function(sequelize, DataTypes) {
 module.exports.transcript_count_no_assoc_migration = `
 'use strict';
 
+/**
+ * @module - Migrations to create and to undo a table correpondant to a sequelize model.
+ */
 module.exports = {
 
+  /**
+   * up - Creates a table with the fields specified in the the createTable function.
+   *
+   * @param  {object} queryInterface Used to modify the table in the database.
+   * @param  {object} Sequelize      Sequelize instance with data types included
+   * @return {promise}                Resolved if the table was created successfully, rejected otherwise.
+   */
     up: function(queryInterface, Sequelize) {
         return queryInterface.createTable('transcript_counts', {
 
@@ -745,6 +755,13 @@ module.exports = {
         });
     },
 
+    /**
+     * down - Deletes a table.
+     *
+     * @param  {object} queryInterface Used to modify the table in the database.
+     * @param  {object} Sequelize      Sequelize instance with data types included
+     * @return {promise}                Resolved if the table was deleted successfully, rejected otherwise.
+     */
     down: function(queryInterface, Sequelize) {
         return queryInterface.dropTable('transcript_counts');
     }
