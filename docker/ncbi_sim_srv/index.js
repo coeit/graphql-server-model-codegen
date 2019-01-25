85d2c100a2bf356653b80061855a336b1968ce30
JKolokoltsev <j.kolokoltsev@gmail.com>
2019-01-25 08:16:00 -0600

    basic integration tests added

---
+++ docker/ncbi_sim_srv/index.js
@@ -0,0 +1,21 @@
+
+const Express = require('express');
+const Routes = require("./static_route_db");
+
+var port = process.env.PORT || 3333;
+var base_url = process.env.BASE_URL || "";
+
+var app = Express();
+
+app.listen(port, function () {
+
+    console.log(`The Express server is listening on port ${port}`);
+
+    if(base_url.length !== 0) {
+        console.log(`The Base URL is \"${base_url}\"`);
+    }else{
+        base_url = "/";
+    }
+
+    app.use(`${base_url}`, Routes.routes());
+});
