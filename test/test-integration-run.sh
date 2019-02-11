a158e1a46594803c103b976a012f84a28da848f2
JKolokoltsev <j.kolokoltsev@gmail.com>
2019-02-11 03:14:14 -0600

    closed #46

--- test/test-integration-run.sh
+++ test/test-integration-run.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
+#set the list of images to be removed from the docker
+DELETE_IMAGES=(gql_postgres gql_science_db_graphql_server gql_ncbi_sim_srv)
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -k|--keep-image)
+        DELETE_IMAGES=("${DELETE_IMAGES[@]/$2}") #+=("$2")
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        echo "unknown option: $key"
+        exit 1
+        ;;
+    esac
+done
+
 # Stop the GraphQL web server, delete the Docker containers and volume, but
 # leave the image. Finally delete the generated code.
 function cleanup {
-  docker-compose -f ./docker/docker-compose-test.yml down -v && \
-    rm -rf ./docker/integration_test_run/models && \
-    rm -rf ./docker/integration_test_run/models-webservice && \
-    rm -rf ./docker/integration_test_run/migrations && \
-    rm -rf ./docker/integration_test_run/schemas && \
+
+    docker-compose -f ./docker/docker-compose-test.yml down -v
+
+    for IMAGE in "${DELETE_IMAGES[@]}"
+    do
+        if ! [[ -z "${IMAGE// }" ]]; then
+            IN_ID=`docker images | grep "$IMAGE"`
+            if ! [[ -z "${IN_ID// }" ]]; then
+             echo "Delete image: $IMAGE"
+             echo "$IN_ID" | awk '{print "docker rmi -f " $3}' | sh
+            fi
+        fi
+    done
+
+    rm -rf ./docker/integration_test_run/models
+    rm -rf ./docker/integration_test_run/models-webservice
+    rm -rf ./docker/integration_test_run/migrations
+    rm -rf ./docker/integration_test_run/schemas
     rm -rf ./docker/integration_test_run/resolvers
 }
+
+cleanup
 
 # Generate the code for the integration test models
 node ./index.js generate ./test/integration-test-input ./docker/integration_test_run
